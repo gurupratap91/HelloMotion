ba0be1acbeb19adf215d2eb95fdc03a2ff86b31b
Gurupratap <gurupratap@codeignition.co>
2015-06-22 12:05:00 +0530

    Gurupratap View tutorial done

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -12,10 +12,23 @@ class AppDelegate
     #alert = UIAlertView.new
     #alert.message = "Hello Motion!"
     #alert.show
-    @window = UIWindow.alloc.initWithFrame(CGRectMake(10,10,100,100))
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window. makeKeyAndVisible
     puts "Hello Again"
 
+    @blue_view = UIView.alloc.initWithFrame(CGRectMake(10,10,100,100))
+    @blue_view.backgroundColor = UIColor.blueColor
+    @window.addSubview(@blue_view)
+    puts @blue_view.frame.size.height
+
+    @green_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+    @green_view.backgroundColor = UIColor.greenColor
+    @window.addSubview(@green_view)
+
+    @red_view = UIView.alloc.initWithFrame(CGRectMake(30, 30, 40, 40))
+    @red_view.backgroundColor = UIColor.redColor
+    @blue_view.addSubview(@red_view)
+    
     true
   end
 end
